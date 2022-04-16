0dcc0a5478fa67968ddb9c55752dda092d95c2ed
aymen2782 <aymen.sellaouti@gmail.com>
2022-04-16 13:49:00 +0100

    controller

---
+++ src/Controller/SecondController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecondController extends AbstractController
+{
+    #[Route('/second/{name}/{age}', name: 'app_second')]
+    public function index($name, $age, Request $request, SessionInterface $session): Response
+    {
+        if (!$session->has('number')) {
+            $session->set('number', 1);
+        } else {
+            $session->set('number', $session->get('number')+1);
+        }
+        dump($request);
+        return $this->render('second/index.html.twig', [
+            'controller_name' => 'SecondController',
+            'esm' => $name,
+            'a3mor' => $age
+        ]);
+    }
+}
